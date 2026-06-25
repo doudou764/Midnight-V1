21f578354f6c48d0057fb67123957eb8b1a28fc3
doudou764 <140897081+doudou764@users.noreply.github.com>
2026-06-25 21:46:56 +0200

    Add event listeners for enter and login buttons

---
+++ Script.js
@@ -0,0 +1,7 @@
+document.querySelector(".enter").addEventListener("click", () => {
+  alert("Bienvenue dans Midnight 🌌");
+});
+
+document.querySelector(".login-btn").addEventListener("click", () => {
+  alert("Connexion bientôt disponible 👤");
+});

a98654ba7efe7af8a6511766618c6bcaf49e1a4e
doudou764 <140897081+doudou764@users.noreply.github.com>
2026-06-25 21:58:20 +0200

    Remove alert event listeners from Script.js

--- Script.js
+++ Script.js
@@ -1,7 +1,1 @@
-document.querySelector(".enter").addEventListener("click", () => {
-  alert("Bienvenue dans Midnight 🌌");
-});
 
-document.querySelector(".login-btn").addEventListener("click", () => {
-  alert("Connexion bientôt disponible 👤");
-});
